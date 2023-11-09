8a07a876d708df991b21069625d7e31ef3f0c5bc
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-11-09 14:37:34 -0500

    completed my_min phase1 & 2

--- Execution/Execution_time.rb
+++ Execution/Execution_time.rb
@@ -1,6 +1,19 @@
 
 #phase 1
+def my_min(list) # O(n^2)
 
+    list.each do |num1| # n 
+        min = true 
+        list.each do |num2| #n
+            if num2 < num1 
+                min = false
+            end
+        end
+        return num1 if min == true 
+    end
+end
+
+#phase 2
 # def my_min(list) #O(n)
 
 #     min = list.first #1
@@ -13,32 +26,5 @@
 #     min   #1
 # end
 
-
-#phase 2
-
-def my_min(list) #O(n)
-    min = list.min 
-    return min 
-end
-
-#phase 3
-# def my_min(list) #O(n)
-#     i = 0 #1
-#     min = list.first#1
-
-#     while i < list.length - 1 # n
-#         num = list[i] 
-
-#         if num < min #1
-#             min = num #1
-#         end #
-
-#         i += 1 #1
-#     end
-
-#     min 
-# end
-
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)
-
