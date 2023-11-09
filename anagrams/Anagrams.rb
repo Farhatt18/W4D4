aa5601176ade1b0077a20a753af71f3cd2e09baa
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-11-09 16:46:49 -0500

    completed anangram phase1_phase3

--- anagrams/Anagrams.rb
+++ anagrams/Anagrams.rb
@@ -1,9 +1,38 @@
 
 
-def first_anagram(str1, str2)
+def first_anagram?(str1, str2) O(n)
+    chars = str1.chars.permutation.to_a #n
 
+    chars.each do |subarr| #n
+       return true if subarr.join == str2 #1
+    end
+
+    return false
 end
 
 
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+p first_anagram?("gizmo", "sally")    #=> false
+p first_anagram?("elvis", "lives")    #=> true
+
+def second_anagram?(str1,str2) # O(n)
+
+    str1.each_char do |char| #n
+        idx = str2.index(char)#1
+        return false if idx.nil?
+        str2[idx] = "" 
+    end
+
+    str2.empty?#1
+
+end
+
+puts 
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
+
+def third_anagram?(str1,str2)#O(n)
+    str1.sort == str2.sort
+end
+
+p third_anagram?("gizmo", "sally")    #=> false
+p third_anagram?("elvis", "lives")    #=> true
